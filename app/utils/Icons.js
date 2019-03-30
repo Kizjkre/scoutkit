bb74b574546d279e23820489fb83cc7040737474
Tristan Peng <pengt@catlin.edu>
2019-03-30 14:59:20 -0700

    Add Icons separately to add -Icon suffix

---
+++ app/utils/Icons.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Add,
+  Autorenew,
+  CloudUpload,
+  Help
+} from '@material-ui/icons';
+
+export function AddIcon() {
+	return <Add />;
+}
+
+export function AutorenewIcon() {
+	return <Autorenew />;
+}
+
+export function CloudUploadIcon() {
+  return <CloudUpload />;
+}
+
+export function HelpIcon() {
+	return <Help />;
+}
