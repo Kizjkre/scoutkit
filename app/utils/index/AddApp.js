e1638c29351bc2db0f794edbe47422fd3320ae6c
Tristan Peng <pengt@catlin.edu>
2019-04-08 18:11:34 -0700

    Add implementation of CreateApp, still buggy, will switch to fullscreen modal

--- app/utils/index/AddApp.js
+++ app/utils/index/AddApp.js
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
+import CreateApp from './app/CreateApp';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
-import { AddIcon, AutorenewIcon, CloudUploadIcon } from './../Icons';
+import SwitchApp from './app/SwitchApp';
+import { AddIcon, AutorenewIcon, CloudUploadIcon } from '../Utils';
 
 export default function AddApp(props) {
+  let [openSwitchApp, setOpenSwitchApp] = useState(false);
+  let [openCreateApp, setOpenCreateApp] = useState(false);
   return (
-    <Dialog onClose={ props.onClose } open={ props.open }>
-      <DialogTitle>New App</DialogTitle>
-      <List>
-        <ListItem button>
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary='Create App' />
-        </ListItem>
-        <ListItem button>
-          <ListItemAvatar>
-            <Avatar>
-              <CloudUploadIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary='Load App' />
-        </ListItem>
-        <ListItem button>
-          <ListItemAvatar>
-            <Avatar>
-              <AutorenewIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary='Switch Apps' />
-        </ListItem>
-      </List>
-    </Dialog>
+    <div>
+      <Dialog onClose={ props.onClose } open={ props.open }>
+        <DialogTitle>New App</DialogTitle>
+        <List>
+          <ListItem button onClick={ () => setOpenCreateApp(true) }>
+            <ListItemAvatar>
+              <Avatar>
+                <AddIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary='Create App' />
+          </ListItem>
+          <ListItem button>
+            <ListItemAvatar>
+              <Avatar>
+                <CloudUploadIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary='Load App' />
+          </ListItem>
+          <ListItem button onClick={ () => setOpenSwitchApp(true) }>
+            <ListItemAvatar>
+              <Avatar>
+                <AutorenewIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary='Switch Apps' />
+          </ListItem>
+        </List>
+        <SwitchApp
+          onClose={ () => setOpenSwitchApp(false) }
+          open={ openSwitchApp }
+        />
+      </Dialog>
+      <CreateApp open={ openCreateApp } />
+    </div>
   );
 }
