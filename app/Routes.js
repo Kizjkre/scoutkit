1e28450107a64d59603b728bf7d08936d81f6bb0
Tristan Peng <pengt@catlin.edu>
2019-04-11 20:37:17 -0700

    Add React routing

---
+++ app/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+import routes from './constants/routes';
+import App from './containers/App';
+import Home from './components/Home';
+import Create from './components/Create';
+
+export default () => (
+  <App>
+    <Route component={ Home } exact path={ routes.HOME } />
+  </App>
+);
