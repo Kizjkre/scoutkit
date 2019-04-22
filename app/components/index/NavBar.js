e6f694ddb3b28da28dc1ad08c94b2fc9926bdd56
Tristan Peng <pengt@catlin.edu>
2019-04-22 10:20:22 -0700

    Lint NavBar and add documentation

--- app/components/index/NavBar.js
+++ app/components/index/NavBar.js
@@ -1,42 +1,57 @@
 import React, { useState } from 'react';
-import AppModal from './AppModal';
 import AppBar from '@material-ui/core/AppBar';
 import Fab from '@material-ui/core/Fab';
-import HelpModal from './HelpModal';
 import IconButton from '@material-ui/core/IconButton';
+import PropTypes from 'prop-types';
 import Toolbar from '@material-ui/core/Toolbar';
-import style from '../../constants/style';
 import { withStyles } from '@material-ui/core/styles';
 import { Add as AddIcon, Help as HelpIcon } from '@material-ui/icons';
+import style from '../../constants/style';
+import HelpModal from './HelpModal';
+import AppModal from './AppModal';
 
+NavBar.defaultProps = {
+  classes: {}
+};
+
+NavBar.propTypes = {
+  classes: PropTypes.shape
+};
+
+/**
+ * NavBar component
+ *
+ * Creates the navbar for the homepage, fixed to the bottom
+ * Contains:
+ * - A floating action button to open the app modal (see AppModal.js)
+ * - A help button for using the app TODO: implement
+ */
 function NavBar(props) {
-  let [openAddApp, setOpenAddApp] = useState(false);
-  let [openHelpModal, setOpenHelpModal] = useState(false);
+  const [openAppModal, setOpenAppModal] = useState(false);
+  const [openHelpModal, setOpenHelpModal] = useState(false);
+  const { classes } = props;
   return (
     <>
-      <AppBar className={ props.classes.appBar } position='fixed'>
-        <Toolbar className={ props.classes.navToolbar }>
+      <AppBar className={classes.appBar} position="fixed">
+        <Toolbar className={classes.navToolbar}>
           <Fab
-            className={ props.classes.navFabButton }
-            color='secondary'
-            onClick={ () => setOpenAddApp(true) }
+            className={classes.navFabButton}
+            color="secondary"
+            onClick={() => setOpenAppModal(true)}
           >
             <AddIcon />
           </Fab>
           <IconButton
-            className={ props.classes.btnNavbar }
-            color='inherit'
-            onClick={ () => setOpenHelpModal(true) }
+            className={classes.btnNavbar}
+            color="inherit"
+            onClick={() => setOpenHelpModal(true)}
           >
             <HelpIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
-      <AppModal onClose={ () => setOpenAddApp(false) } open={ openAddApp } />
-      <HelpModal
-        onClose={ () => setOpenHelpModal(false) }
-        open={ openHelpModal }
-      />
+      <AppModal onClose={() => setOpenAppModal(false)} open={openAppModal} />
+      <HelpModal onClose={() => setOpenHelpModal(false)} open={openHelpModal} />
     </>
   );
 }
