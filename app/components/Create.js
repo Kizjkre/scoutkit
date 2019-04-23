b7fc8bb2815c752113fbb31031025b7195d895cc
Tristan Peng <pengt@catlin.edu>
2019-04-23 11:19:00 -0700

    Lint Create and add documentation

--- app/components/Create.js
+++ app/components/Create.js
@@ -2,33 +2,53 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
-import fs from 'fs';
-import style from './../constants/style';
 import { withStyles } from '@material-ui/core';
 import { Add as BackIcon } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
-import { dataPath } from '../constants/appData';
+import PropTypes from 'prop-types';
+import style from '../constants/style';
 
+Create.defaultProps = {
+  classes: {}
+};
+
+Create.propTypes = {
+  classes: PropTypes.shape
+};
+
+/**
+ * Create component
+ *
+ * The /create page, for creating a new app
+ * Like ./index/NavBar.js, navbar is fixed to the bottom
+ * - Contains a back button to go back to '/'
+ * TODO: get name working, in progress save
+ */
 function Create(props) {
   save();
+  const { classes } = props;
   return (
-    <div>
-      <AppBar className={ props.classes.appBar } position='fixed'>
-        <Toolbar className={ props.classes.navToolbar }>
-          <Link className={ props.classes.btnNavbar } to='/'>
-            <IconButton color='inherit'>
-                <BackIcon />
+    <>
+      <AppBar className={classes.appBar} position="fixed">
+        <Toolbar className={classes.navToolbar}>
+          <Link className={classes.btnNavbar} color="inherit" to="/">
+            <IconButton color="inherit">
+              <BackIcon />
             </IconButton>
           </Link>
         </Toolbar>
       </AppBar>
-    </div>
+    </>
   );
 }
 
+/**
+ * Saves the app by create a directory and name.txt file
+ * TODO: fix
+ */
 function save() {
   console.log(window.location.href);
-	// fs.writeFileSync(`${ dataPath }/${  }`);
+  // fs.writeFileSync(`${ dataPath }/${  }`);
 }
 
 export default withStyles(style)(Create);
