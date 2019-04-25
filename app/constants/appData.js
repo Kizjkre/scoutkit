f02dc258d5552754854dad68fbccd357483fbd31
Tristan Peng <pengt@catlin.edu>
2019-04-24 21:32:20 -0700

    Lint appData and add documentation

--- app/constants/appData.js
+++ app/constants/appData.js
@@ -3,22 +3,37 @@ import fs from 'fs';
 
 const { app } = remote;
 
-export const dataPath = `${ app.getPath('appData') }/ScoutKit/data`;
+/**
+ * Path to where the data is stored
+ * Uses the /Application Support/ folder
+ */
+export const dataPath = `${app.getPath('appData')}/ScoutKit/data`;
 
+/**
+ * Lists all saved apps
+ * Returns an object with two properties
+ * - Array of app paths
+ * - Array for app names
+ */
 export function listApps() {
-  let
-    apps = fs.readdirSync(dataPath).filter(e => e.includes('app-')),
-    names = [];
-  apps.length !== 0 && apps.forEach(e => names.push(
-    fs.readFileSync(`${ dataPath }/${ e }/name.txt`, 'utf8')
-  ));
-  return { dir: apps, names: names };
+  const apps = fs.readdirSync(dataPath).filter(e => e.includes('app-'));
+  const names = [];
+  if (apps.length !== 0) {
+    apps.forEach(e =>
+      names.push(fs.readFileSync(`${dataPath}/${e}/name.txt`, 'utf8'))
+    );
+  }
+  return { dir: apps, names };
 }
 
+/**
+ * rm -rf yeet!
+ * Recursively deletes a directory
+ */
 export function rmrf(path) {
   if (fs.existsSync(path)) {
     fs.readdirSync(path).forEach(file => {
-      var curPath = `${ path }/${ file }`;
+      const curPath = `${path}/${file}`;
       if (fs.lstatSync(curPath).isDirectory()) {
         rmrf(curPath);
       } else {
