b941c0ef7b3f661cafc026171b35c57574d672b5
Tristan Peng <pengt@catlin.edu>
2019-04-26 08:58:01 -0700

    Lint SwitchApp and add documentation

--- app/components/index/app/SwitchApp.js
+++ app/components/index/app/SwitchApp.js
@@ -6,33 +6,52 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import { withStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
 import style from '../../../constants/style';
 import { listApps } from '../../../constants/appData';
-import { withStyles } from '@material-ui/core';
 
+SwitchApp.defaultProps = {
+  classes: {},
+  onClose: () => {},
+  open: false
+};
+
+SwitchApp.propTypes = {
+  classes: PropTypes.shape,
+  onClose: PropTypes.func,
+  open: PropTypes.bool
+};
+
+/**
+ * SwitchApp component
+ *
+ * Modal that allows the user to switch an app
+ */
 function SwitchApp(props) {
+  const { classes, onClose, open } = props;
   let apps = listApps();
   useEffect(() => {
     apps = listApps();
   });
   return (
-    <Dialog onClose={ props.onClose } open={ props.open }>
+    <Dialog onClose={onClose} open={open}>
       <DialogTitle>Switch Apps</DialogTitle>
-      {
-        apps.names.length ?
-          apps.names.map(n => (
-            <List key={ n }>
-              <ListItem button>
-                <ListItemText primary={ n } />
-              </ListItem>
-            </List>
-          )) :
-          <DialogContent>
-            <DialogContentText className={ props.classes.center }>
-              No apps
-            </DialogContentText>
-          </DialogContent>
-      }
+      {apps.names.length ? (
+        apps.names.map(n => (
+          <List key={n}>
+            <ListItem button>
+              <ListItemText primary={n} />
+            </ListItem>
+          </List>
+        ))
+      ) : (
+        <DialogContent>
+          <DialogContentText className={classes.center}>
+            No apps
+          </DialogContentText>
+        </DialogContent>
+      )}
     </Dialog>
   );
 }
