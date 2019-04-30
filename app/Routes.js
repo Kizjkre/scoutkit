92d366bdb5d72e1e181ee38136db4d7f91a7a28d
Tristan Peng <pengt@catlin.edu>
2019-04-29 21:00:56 -0700

    Simplify Routes

--- app/Routes.js
+++ app/Routes.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router';
 import routes from './constants/routes';
-import App from './containers/App';
 import Home from './components/Home';
 import Create from './components/Create';
 
 export default () => (
-  <App>
+  <>
     <Route component={Home} exact path={routes.HOME} />
     <Route component={Create} path={routes.CREATE} />
-  </App>
+  </>
 );
